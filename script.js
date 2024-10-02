261b88ea9d34526a734439a2b4a7973b5bc61732
Christodoulos Paschalis <christodoulospasxalhs@gmail.com>
2024-10-02 13:30:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+
+const quotes = [
+    "The best way to predict the future is to invent it.",
+    "Life is 10% what happens to us and 90% how we react to it.",
+    "The only limit to our realization of tomorrow is our doubts of today.",
+    "Do something today that your future self will thank you for."
+]
