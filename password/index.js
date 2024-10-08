66368338e7d1df4c0018c7326d75eb1871dc7159
Christodoulos Paschalis <christodoulospasxalhs@gmail.com>
2024-10-08 15:12:17 +0300

    password generator added

--- password/index.js
+++ password/index.js
@@ -3,8 +3,13 @@ const pw1 = document.getElementById('password-one');
 const pw2 = document.getElementById('password-two');
 let length = prompt("Give me your password length (8-16):");
 
+if (length < 8 || length > 16) {
+    alert("Password length should be between 8 and 16 characters.");
+    length = prompt("Give me your password length (8-16):");
+}
 
-function generatePassword(length,charset){
+
+function generatePassword(length, charset) {
     let password = '';
     for (let i = 0; i < length; i++) {
         let randomIndex = Math.floor(Math.random() * charset.length);
@@ -13,9 +18,15 @@ function generatePassword(length,charset){
     return password;
 }
 
-
-generate.addEventListener('click',function(){
-    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";    
-    pw1.value = generatePassword(length,charset);  
-    pw2.value = generatePassword(length,charset); 
+generate.addEventListener('click', function () {
+    let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+    pw1.value = generatePassword(length, charset);
+    pw2.value = generatePassword(length, charset);
 })
+
+
+
+
+
+
+
